9694371067eb5875411761111360a017150f8d4e
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2024-03-20 22:09:24 +0100

    design-system typography settings tailwind created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { Seo } from "@/ui/components/seo";
+import { Typographiy } from "@/ui/design-system/typography/typography";
+
 export default function Home() {
-  return <div className="bg-red-500">Coders Monkeys</div>;
+  return (
+    <>
+      <Seo title="Coders Monkeys" description="Description..." />
+      <div className="space-y-5">
+        <Typographiy variant="display" component="h1" theme="primary" weigth="medium">
+          Coders Monkeys
+        </Typographiy>
+      </div>
+    </>
+  );
 }
